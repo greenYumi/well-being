9e3a798211d26e71531ff832013114e1f744bc6b
rio.lianto <rio.lianto@students.mikroskil.ac.id>
2025-05-09 00:46:47 +0700

    animasi kan icon, belum siap

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:well_being/provider/app_resource_provider.dart';
-import 'package:well_being/provider/general_provider.dart';
 import 'package:well_being/provider/user_resource_provider.dart';
 import 'package:well_being/screen/main_screen.dart';
-import 'package:well_being/screen/sport_screen.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => GeneralProvider()),
         ChangeNotifierProvider(create: (context) => AppResourceProvider()),
         ChangeNotifierProvider(create: (context) => UserResourceProvider()),
       ],
--- lib/provider/app_resource_provider.dart
+++ lib/provider/app_resource_provider.dart
@@ -5,4 +5,17 @@ class AppResourceProvider with ChangeNotifier {
 
   // ----- APPBAR
   // Icon_size = 36;
+  double iconFontSize = 20;
+
+  Future<void> trigerBump() async {
+    iconFontSize = 34;
+    notifyListeners();
+    print("icon besar");
+
+    await Future.delayed(Duration(seconds: 1));
+
+    iconFontSize = 20;
+    notifyListeners();
+    print("icon kecil");
+  }
 }
